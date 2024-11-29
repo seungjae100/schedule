e479fad7576ccd45afe92aa111c55b40ebe40d7e
seungjae100 <seungjaebaekmail@gmail.com>
2024-11-29 20:32:32 +0900

    feat/reactUser-#15 : 로그인, 회원가입페이지 구현 css 수정예정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import LoginPage from "./pages/user/LoginPage";
+import SignupPage from "./pages/user/SignupPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* 기본 경로를 로그인 페이지로 리다이렉트 */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* 인증 관련 라우트*/}
+        <Route path="/users/login" element={<LoginPage/> } />
+        <Route path="/users/signup" element={<SignupPage/> } />
+      </Routes>
+    </Router>
   );
 }
 
